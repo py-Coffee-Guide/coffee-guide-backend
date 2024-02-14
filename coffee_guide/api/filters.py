d5e6edf79bdbc85f7df61f3439be2fe7e4a12193
DanilLTC <yakushev.danil@gmail.com>
2024-02-14 21:20:08 +0300

    fix problem with filter, gmail and import_data

--- coffee_guide/api/filters.py
+++ coffee_guide/api/filters.py
@@ -1,24 +1,34 @@
 import django_filters
 
-from cafe.models import Cafe, Tag
+from cafe.models import Cafe, Alternative
 
 
 class CafeFilter(django_filters.FilterSet):
     """
-    /api/v1/cafe/?name=your_filter_name
-    /api/v1/cafe/?alternatives=your_alternative_name
-    /api/v1/cafe/?roasters=your_roaster_name
-    /api/v1/cafe/?tags=your_tag_name
-    /api/v1/cafe/?address=your_address
+    /api/v1/cafes/?name=your_filter_name
+    /api/v1/cafes/?alternatives=v60&alternatives=chemex&alternatives=aeropress&alternatives=french_press
+    /api/v1/cafes/?roasters=Submarine&roasters=The_Welder_Catherine
+    /api/v1/cafes/?tags=La_Marzocco
+    /api/v1/cafes/?address=your_address
     """
 
     tags = django_filters.ModelMultipleChoiceFilter(
-        field_name="tags__name", to_field_name="name", queryset=Tag.objects.all()
+        field_name="tags__slug",
+        to_field_name="name",
     )
-    address = django_filters.CharFilter(field_name="address__name", lookup_expr="icontains")
-    name = django_filters.CharFilter(field_name="name", lookup_expr="icontains")
-    alternatives = django_filters.CharFilter(field_name="alternatives__name", lookup_expr="icontains")
-    roasters = django_filters.CharFilter(field_name="roasters__name", lookup_expr="icontains")
+    address = django_filters.CharFilter(
+        field_name="address__name", lookup_expr="icontains"
+    )
+    name = django_filters.CharFilter(
+        field_name="name",
+        lookup_expr="icontains",
+    )
+    alternatives = django_filters.CharFilter(
+        field_name="alternatives__slug", lookup_expr="icontains", queryset=Alternative.objects.all()
+    )
+    roasters = django_filters.CharFilter(
+        field_name="roasters__slug", lookup_expr="icontains"
+    )
 
     class Meta:
         model = Cafe
